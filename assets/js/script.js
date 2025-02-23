48461648428a5ac9c85ed3db3f4b1c8c058d8f13
Mithanshu Hedau <144697427+MithanshuHedau@users.noreply.github.com>
2025-02-23 15:50:50 +0530

    Final Update 3rd Year

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-'use strict';
-
-
+"use strict";
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -31,29 +29,27 @@ const modalText = document.querySelector("[data-modal-text]");
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
 
     testimonialsModalFunc();
-
   });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,17 +57,17 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+  elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
@@ -79,9 +75,7 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -89,18 +83,14 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
-}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
   filterBtn[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
@@ -108,12 +98,8 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -123,18 +109,14 @@ const formBtn = document.querySelector("[data-form-btn]");
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-
     // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
-
   });
 }
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -143,7 +125,6 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
@@ -154,74 +135,73 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[i].classList.remove("active");
       }
     }
-
   });
 }
 
 gsap.registerPlugin(ScrollTrigger);
-var vr=gsap.timeline();
-vr.from("#myphoto",{
-  opacity:0,
-  y:100,
-  scale:1.2,
-  duration:1,
-})
-vr.from(".name",{
-  opacity:0,
+var vr = gsap.timeline();
+vr.from("#myphoto", {
+  opacity: 0,
+  y: 100,
+  scale: 1.2,
+  duration: 1,
+});
+vr.from(".name", {
+  opacity: 0,
   // y:100,
-  scale:1.2,
-  duration:0.8,
-})
-gsap.from(".main-content",{
-  opacity:0,
-  scale:1.2,
-  duration:1,
-})
+  scale: 1.2,
+  duration: 0.8,
+});
+gsap.from(".main-content", {
+  opacity: 0,
+  scale: 1.2,
+  duration: 1,
+});
 
-gsap.from(".projects",{
-  opacity:0,
-  duration:3,
-  stagger:0.9,
-})
+gsap.from(".projects", {
+  opacity: 0,
+  duration: 3,
+  stagger: 0.9,
+});
 gsap.from(".service-item", {
   opacity: 0,
-  y: 50, 
+  y: 50,
   stagger: 0.7,
   scrollTrigger: {
-    trigger: ".service", 
+    trigger: ".service",
     scroller: "body",
-    start: "top 60%", 
-    end: "bottom 60%", 
-    markers: false
-  }
+    start: "top 60%",
+    end: "bottom 60%",
+    markers: false,
+  },
 });
 
 gsap.from(".clients-title", {
   opacity: 0,
-  duration:1.2,
-  x:-150, 
+  duration: 1.2,
+  x: -150,
   stagger: 0.5,
   scrollTrigger: {
-    trigger: ".clients-title", 
+    trigger: ".clients-title",
     scroller: "body",
-    start: "top 80%", 
-    end: "bottom 60%", 
+    start: "top 80%",
+    end: "bottom 60%",
     // markers: true
-  }
+  },
 });
 
 gsap.from(".clients-list", {
   opacity: 0,
-  duration:2,
-  // x:-150, 
-  y:50,
+  duration: 2,
+  // x:-150,
+  y: 50,
   stagger: 1.3,
-  scrub:2,
+  scrub: 2,
   scrollTrigger: {
-    trigger: ".clients-list", 
+    trigger: ".clients-list",
     scroller: "body",
-    start: "top 80%", 
-    end: "bottom 60%", 
+    start: "top 80%",
+    end: "bottom 60%",
     // markers: true
-  }
+  },
 });
